8395de4593f7b9a3e046be1561288c36afc13696
EL-Projects <64143348+EL-Projects@users.noreply.github.com>
2024-03-07 21:16:26 +0200

    add touch slide

--- js/slider.js
+++ js/slider.js
@@ -32,3 +32,63 @@ document.addEventListener("DOMContentLoaded", function () {
     prevBtn.addEventListener("click", prevSlide);
   });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const carouselIds = [
+    "carousel1",
+    "carousel2",
+    "carousel3",
+    "carousel4",
+    "carousel5",
+  ];
+
+  carouselIds.forEach(function (carouselId) {
+    const carousel = document.getElementById(carouselId);
+    const prevBtn = document.getElementById(`${carouselId}-prev`);
+    const nextBtn = document.getElementById(`${carouselId}-next`);
+    let currentIndex = 0;
+    let touchStartX = 0;
+    let touchEndX = 0;
+
+    function showCurrentIndex() {
+      carousel.style.transform = `translateX(-${currentIndex * 100}%)`;
+    }
+
+    function nextSlide() {
+      currentIndex = (currentIndex + 1) % carousel.children.length;
+      showCurrentIndex();
+    }
+
+    function prevSlide() {
+      currentIndex =
+        (currentIndex - 1 + carousel.children.length) %
+        carousel.children.length;
+      showCurrentIndex();
+    }
+
+    function handleTouchStart(event) {
+      touchStartX = event.touches[0].clientX;
+    }
+
+    function handleTouchMove(event) {
+      touchEndX = event.touches[0].clientX;
+    }
+
+    function handleTouchEnd() {
+      const diff = touchEndX - touchStartX;
+      if (Math.abs(diff) > 50) {
+        if (diff > 0) {
+          prevSlide();
+        } else {
+          nextSlide();
+        }
+      }
+    }
+
+    nextBtn.addEventListener("click", nextSlide);
+    prevBtn.addEventListener("click", prevSlide);
+    carousel.addEventListener("touchstart", handleTouchStart);
+    carousel.addEventListener("touchmove", handleTouchMove);
+    carousel.addEventListener("touchend", handleTouchEnd);
+  });
+});

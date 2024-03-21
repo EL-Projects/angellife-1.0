d75160336a8706bb769aa0e193062915dea04b35
EL-Projects <64143348+EL-Projects@users.noreply.github.com>
2024-03-21 10:06:36 +0200

    mail

---
+++ mail.php
@@ -0,0 +1,47 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'phpMailer/Exception.php';
+require 'phpMailer/PHPMailer.php';
+require 'phpMailer/SMTP.php';
+
+// Проверяем, была ли отправлена форма
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Получаем данные из формы
+    $name = $_POST['user_name'];
+    $phone = $_POST['user_phone'];
+    $email = $_POST['user_email'];
+    $comment = $_POST['user_comment'];
+
+    // Создаем объект PHPMailer
+    $mail = new PHPMailer(true);
+
+    try {
+        // Настройки сервера SMTP
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'elukjanskis@gmail.com'; // Ваш адрес электронной почты Gmail
+        $mail->Password = 'wyde cgbt hkog fwwq'; // Ваш пароль приложения Gmail
+        $mail->SMTPSecure = 'tls';
+        $mail->Port = 587;
+
+        // Настройки письма
+        $mail->setFrom($email, $name);
+        $mail->addAddress('angellife.kyiv@gmail.com'); // Адрес получателя
+        $mail->Subject = 'Новое сообщение из формы обратной связи';
+        $mail->Body = "Имя: $name\nТелефон: $phone\nEmail: $email\n\nКомментарий:\n$comment";
+
+        // Отправляем письмо
+        $mail->send();
+        echo 'Ваше сообщение было успешно отправлено!';
+    } catch (Exception $e) {
+        echo "Ошибка при отправке сообщения: {$mail->ErrorInfo}";
+    }
+} else {
+    // Если форма не была отправлена, вы можете сделать здесь что-то еще, например, перенаправить пользователя на другую страницу
+    // header("Location: другая_страница.php");
+    // exit;
+}
+?>
